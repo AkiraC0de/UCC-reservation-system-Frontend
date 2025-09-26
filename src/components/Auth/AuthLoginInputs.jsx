ee5a3876b93d5df731ec4abedf3428e4c0f53d2f
AkiraC0de <ravenakira22@gmail.com>
2025-09-26 21:35:59 +0800

    Added Show Password buttons

---
+++ src/components/Auth/AuthLoginInputs.jsx
@@ -0,0 +1,19 @@
+import Input from "../Shared/Input"
+
+const AuthLoginInputs = () => {
+  return (
+    <div className="flex flex-col gap-6">
+        <Input
+          label="Email *"
+          placeholder="Enter your email"
+          type="email"
+        />
+        <Input
+          label="Password *"
+          placeholder="Enter your password"
+          type="password"
+        />
+      </div>
+  )
+}
+export default AuthLoginInputs
---
+++ src/components/Auth/SubmitBtn.jsx
@@ -0,0 +1,8 @@
+const SubmitBtn = () => {
+  return (
+    <button className="bg-green-gradient-2 text-sm text-white font-semibold tracking-wide py-2 rounded-3xl">
+      SIGN UP
+    </button>
+  )
+}
+export default SubmitBtn
