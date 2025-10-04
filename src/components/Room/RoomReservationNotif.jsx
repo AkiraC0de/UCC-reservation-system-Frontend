0792f11c76b81ce81675043253a4908c9ed6fc72
AkiraC0de <ravenakira22@gmail.com>
2025-10-04 08:50:27 +0800

    New: Notification after sending a reservation

---
+++ src/components/Room/RoomReservationNotif.jsx
@@ -0,0 +1,13 @@
+import useRoom from "../../hooks/useRoom"
+import ConfirmIcon from "../Shared/Icons/ConfirmIcon"
+
+const RoomReservationNotif = () => {
+  const {serverResponse} = useRoom()
+  return (
+    <div className="absolute top-18 right-1/2 translate-x-1/2 bg-white p-2 flex flex-col items-center gap-2 w-50 rounded-lg shadow-lg anim-fade-pop-top">
+      <ConfirmIcon className="w-10 fill-green-500"/>
+      <p className="text-[12px] font-semibold text-center text-black-text">{serverResponse.message}</p>
+    </div>
+  )
+}
+export default RoomReservationNotif
