e91075a6c59059b27a1322717cb6f830c72f436a
Mary Sembrero <sembrero.maryjoybscs2024@gmail.com>
2025-11-07 22:59:49 +0800

    Added responsive design to homepage.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import RightSection from "../components/Home/RightSection"
 const Home = () => {
   return (
     <main>
-      <div className="grid grid-cols-2 px-10 py-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 px-10 py-6">
         <LeftSection/>
         <RightSection/>
       </div>
