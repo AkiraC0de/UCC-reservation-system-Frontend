f7e3672411282e5bd26d84069f5ea9753650e9a6
Mary Sembrero <sembrero.maryjoybscs2024@gmail.com>
2025-11-07 23:23:42 +0800

    Updated navUser buttons' styles.

--- src/components/Navigation/Notification/Notification.jsx
+++ src/components/Navigation/Notification/Notification.jsx
@@ -9,12 +9,17 @@ export default function Notification() {
     setShowNotifications(prev=> !prev);
   }
 
+  const notifClickedStyles = `border-2 mx-2 text-green-700 text-xs font-semibold rounded-3xl px-3 py-1.5 cursor-pointer bg-green-700/40 transition-all duration-300`
+
+  const notifButtonStyles = `border-2 mx-2 text-green-700 text-xs font-semibold rounded-3xl px-3 py-1.5 cursor-pointer hover:bg-green-700/20 transition-all duration-300`
+
+
   return (
     <>
         <div className='relative '>
               <button 
               onClick={toggleShowNotification}
-              className="border-2 mx-2 text-green-700 text-xs font-semibold rounded-3xl px-3 py-1.5 cursor-pointer hover:bg-gray-200/50 transition-all duration-300">
+              className={showNotifications ? `${notifClickedStyles} `: `${notifButtonStyles}`}>
                   <img
                   src="icons/notifIcon.svg"
                   width={15}
@@ -23,7 +28,7 @@ export default function Notification() {
 
               {showNotifications &&
               <NotificationCard
-              className="absolute pop-animation -left-24 -translate-x-1/2 mt-2 border border-green-800/50 h-[88vh] w-[18rem] overflow-y-auto rounded-xl shadow-lg bg-white p-2"
+              className="absolute pop-animation left-1/2 -translate-x-3/4 md:-left-24 md:-translate-x-1/4 mt-2 border border-green-800/50 h-[88vh] w-[20rem] md:w-[18rem] overflow-y-auto rounded-xl shadow-lg bg-white p-2"
             />
 
               }
